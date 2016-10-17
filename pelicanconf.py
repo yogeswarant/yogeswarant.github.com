2d51d9d2dcd9622a5418f9d06da17378c3d9c3e5
Yogeswaran Thulasidoss <yogeeswaran@gmail.com>
2016-10-17 06:58:37 +0530

    First release with previously written articles

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,16 +20,57 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Planet Python', 'http://planetpython.org/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'http://twitter.com/yogeswarant'),
+        ('github', 'https://github.com/yogeswarant'),
+        ('LinkedIn', 'https://in.linkedin.com/in/yogeswaran-thulasidoss-ab29a22a'),)
+
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+TAG_URL = 'tags/{slug}.html'
+TAG_SAVE_AS = 'tags/{slug}.html'
+TAGS_URL = 'tags.html'
+
+#bootstrap customization
+STATIC_PATHS = ['extras/custom.css']
+EXTRA_PATH_METADATA = {
+'extras/custom.css': {'path': 'static/custom.css'}
+}
+CUSTOM_CSS = 'static/custom.css'
+
+
+# theme settings
+THEME='themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'cosmo' #'lumen' #'sandstone' #'cosmo'
+PYGMENTS_STYLE = 'monokai'
+ABOUT_ME = 'Passionate programmer.  Proud father.  Prospective agriculturist.'
+SITELOGO = 'images/logo.jpeg'
+SITELOGO_SIZE = 24
+FAVICON = 'images/logo.jpeg'
+
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
+
+# plugins
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['simple_footnotes', 'extract_toc', 'feed_summary']
+FEED_USE_SUMMARY = True
+SUMMARY_MAX_LENGTH = 100
+
+MD_EXTENSIONS = ['toc', 'fenced_code', 'codehilite(css_class=highlight)', 'extra']
+
+#PLUGINS = PLUGINS + ['disqus_static']
+#DISQUS_SITENAME = 'everyogi-in'
+ADDTHIS_PROFILE ='ra-5800fd37485cdfec'
+ADDTHIS_DATA_TRACK_ADDRESSBAR = False
